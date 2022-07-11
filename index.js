b151f77e71bf1de6652de2b57c7a2b0f35362cf9
Ahmad Software <99181319+Ahmad-Div@users.noreply.github.com>
2022-07-11 12:00:56 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+$(document).ready(function () {
+    $(".mobile-nav").toggle({
+        "display": "none"
+    })
+    let dark = $("#icon").click(function () {
+        if (dark.hasClass("fa-moon")) {
+            dark.removeClass("fa-moon");
+            dark.addClass("fa-sun");
+            $("body").addClass("dark-theme");
+            $(".fa-bars").css({
+                "color": "white"
+            })
+        } else {
+            dark.removeClass("fa-sun");
+            dark.addClass("fa-moon");
+            $("body").removeClass("dark-theme");
+            $(".fa-bars").css({
+                "color": "black"
+            })
+        }
+    })
+
+
+    $(".fa-bars").click(function () {
+        $(".mobile-nav").toggle({
+            "display": "flex"
+        })
+    })
+
+    $(".mobile-nav a").click(function () {
+        $(".mobile-nav").css({
+            "display": "none"
+        })
+    })
+
+
+    $(".navigation a").click(function () {
+        $(".navigation a").each(function () {
+            $(this).children().removeClass("current-section");
+        })
+        $(this).children().addClass("current-section");
+    })
+
+    $(".logo").click(function () {
+        $(".navigation a").each(function () {
+            $(this).children().removeClass("current-section");
+        })
+    })
+
+})
